73d6c8cda9f1bbecf8528fb63bef91f79e34a813
Robert Wagner <robert.wagner@octopus.com>
2016-11-03 11:37:55 +1000

    Swapped out NETStandard.Library for the minimum libs required

--- source/Octostache/Properties/AssemblyInfo.cs
+++ source/Octostache/Properties/AssemblyInfo.cs
@@ -19,9 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c5cb0670-698d-489d-bffc-31735f521f42")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
