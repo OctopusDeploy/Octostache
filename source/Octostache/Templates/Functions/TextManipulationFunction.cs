a9494728672b7fdf17eefa38439f4ce0a4376c5b
Ryan Anthony <ranthony@slyngshothealth.com>
2018-06-13 15:16:25 -0600

    Implement { something | ToBase64} functionality

---
+++ source/Octostache/Templates/Functions/TextManipulationFunction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Octostache.Templates.Functions
+{
+    internal class TextManipulationFunction
+    {
+        public static string ToBase64(string argument, string[] options)
+        {
+            if (options.Any() || argument == null)
+            {
+                return null;
+            }
+            var argumentBytes = Encoding.UTF8.GetBytes(argument);
+            return Convert.ToBase64String(argumentBytes);
+        }
+    }
+}
