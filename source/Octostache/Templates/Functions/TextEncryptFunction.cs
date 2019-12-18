2bdbcbc16e64065d609f69395b01dfece79d51a2
Eric Roth <eric.roth@feisystems.com>
2019-12-18 11:00:10 -0500

    Added Ability to encrypt variables with RSA Certificate.

---
+++ source/Octostache/Templates/Functions/TextEncryptFunction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Octostache.Templates.Functions
+{
+    internal class TextEncryptFunction
+    {
+        public static string RSAEncrypt(string data, string[] arguments)
+        {
+            if (arguments == null || arguments.Length == 0 || arguments.Length > 2)
+            {
+                return null;
+            }
+
+            try
+            {
+
+                var certificate = new X509Certificate2(Convert.FromBase64String(arguments[0]));
+#if NET40
+                var publicKey = (RSACryptoServiceProvider)certificate.PublicKey.Key;
+#else
+                var publicKey = certificate.GetRSAPublicKey();
+#endif
+                using (publicKey)
+                {
+                    var encoding = Encoding.GetEncoding(arguments.Length > 1 ? arguments[1] : "utf-8");
+                    var byteData = encoding.GetBytes(data);
+#if NET40
+                    var encryptedBytes = publicKey.Encrypt(byteData, false);
+#else
+                    var encryptedBytes = publicKey.Encrypt(byteData, RSAEncryptionPadding.OaepSHA1);
+#endif
+                    return Convert.ToBase64String(encryptedBytes);
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            return null;
+        }
+    }
+}
