414c497104e696e15fb2132f796232ca2a90f863
Shannon Lewis <slewis74@gmail.com>
2020-09-17 14:33:45 +1000

    made backing field an auto property

--- source/Octostache/Templates/Binding.cs
+++ source/Octostache/Templates/Binding.cs
@@ -5,17 +5,15 @@ namespace Octostache.Templates
 {
     class Binding : Dictionary<string, Binding>
     {
-        readonly Dictionary<string, Binding> indexable; 
-
         public Binding(string? item = null)
             : base(StringComparer.OrdinalIgnoreCase)
         {
             Item = item;
-            indexable = new Dictionary<string, Binding>(StringComparer.OrdinalIgnoreCase);
+            Indexable = new Dictionary<string, Binding>(StringComparer.OrdinalIgnoreCase);
         }
 
         public string? Item { get; set; }
 
-        public Dictionary<string, Binding> Indexable => indexable;
+        public Dictionary<string, Binding> Indexable { get; }
     }
 }
