7ea2c5fbe93c579925eccfe67b1ccf0fdf356962
Egor Pavlikhin <egor.pavlikhin@gmail.com>
2021-08-10 17:19:15 +1000

    Verify that a parsed template can be reliably converted back to string

--- source/Octostache.Tests/ParserFixture.cs
+++ source/Octostache.Tests/ParserFixture.cs
@@ -39,5 +39,28 @@ namespace Octostache.Tests
             result.Should().NotContain("item");
             result.Should().NotContain("item.Address");
         }
+
+        [Fact]
+        public void ParsedTemplateIsConvertibleBackToString()
+        {
+            // This template is using the internal syntax, full form syntax, without omitting any spaces or #{else} statements.
+            // For example, #{if test}#{/if} has a full syntax of #{if test}#{else}#{/if}
+            var template = @"
+                #{var}
+                #{filter | Format #{option}}
+                #{nestedParent[#{nestedChild}]}
+                #{if ifvar}#{if ifnested}#{else}debug#{/if}#{else}#{/if}
+                #{if comparison != ""value"" }true#{else}#{/if}
+                #{each item in List}
+                   #{item.Address}
+                #{/each}
+                ##{ignored}
+            ";
+
+            TemplateParser.TryParseTemplate(template, out var parsedTemplate, out string error).Should().BeTrue();
+            string.IsNullOrEmpty(error).Should().BeTrue();
+            parsedTemplate.Should().NotBeNull();
+            parsedTemplate.ToString().Should().Be(template);
+        }
     }
 }
