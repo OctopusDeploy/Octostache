1bf1083555dda6d760ad9f6782712ec7be3d1a00
Egor Pavlikhin <egor.pavlikhin@octopus.com>
2021-08-11 13:36:47 +1000

    Add quotes around TextToken for string equality comparison and function expressions (#61)
    * Put quotes back in around string equality comparison
    * Add double quotes to text tokens in function expressions.

--- source/Octostache.Tests/ParserFixture.cs
+++ source/Octostache.Tests/ParserFixture.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Linq;
+using FluentAssertions;
+using Newtonsoft.Json;
 using Octostache.Templates;
 using Xunit;
 
@@ -38,6 +40,47 @@ namespace Octostache.Tests
             result.Should().NotContain("ignored");
             result.Should().NotContain("item");
             result.Should().NotContain("item.Address");
+        }
+
+        [Fact]
+        public void ParsedTemplateIsConvertibleBackToString()
+        {
+            // This test verifies that it is possible to convert a parsed template to a string
+            // and then find individual tokens within the template by calling ToString on each token individually
+            
+            var template = @"
+                #{var}
+                #{filter | Format #{option}}
+                #{nestedParent[#{nestedChild}]}
+                #{if ifvar}#{if ifnested}#{else}debug#{/if}#{/if}
+                #{if comparison != ""value"" }true#{/if}
+                #{each item in List}
+                   #{item.Address}
+                #{/each}
+                ##{ignored}
+                #{MyVar | Match ""a b""}
+                #{MyVar | StartsWith Ab}
+                #{MyVar | Match ""ab[0-9]+""}
+                #{MyVar | Match #{pattern}}
+                #{MyVar | Contains "" b(""}
+                #{ | NowDateUtc}
+                #{MyVar | UriPart IsFile}
+            ";
+
+            TemplateParser.TryParseTemplate(template, out var parsedTemplate, out string _);
+            parsedTemplate.ToString().Should().NotBeEmpty();
+            
+            // We convert the template back to the string representation and then remove individual parsed expressions until there is nothing left
+            // The purpose is to verify that both methods (on template and tokens) are deterministic and produce equivalent string representations
+            string templateConvertedBackToString = parsedTemplate.ToString();
+            foreach (var templateToken in parsedTemplate.Tokens)
+            {
+                if (!string.IsNullOrWhiteSpace(templateToken.ToString()))
+                    templateConvertedBackToString = templateConvertedBackToString.Replace(templateToken.ToString(),  "");
+            }
+
+            templateConvertedBackToString = templateConvertedBackToString.Replace("\r\n", "");
+            templateConvertedBackToString.Trim().Should().BeEmpty();
         }
     }
 }
