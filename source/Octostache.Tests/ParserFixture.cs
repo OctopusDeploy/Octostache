80fcc391dedc9e876fe96d343fa6b2408be4574d
Egor Pavlikhin <egor.pavlikhin@gmail.com>
2021-08-11 12:55:33 +1000

    Add double quotes to text tokens in function expressions.

--- source/Octostache.Tests/ParserFixture.cs
+++ source/Octostache.Tests/ParserFixture.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Linq;
+using FluentAssertions;
+using Newtonsoft.Json;
 using Octostache.Templates;
 using Xunit;
 
@@ -43,24 +45,40 @@ namespace Octostache.Tests
         [Fact]
         public void ParsedTemplateIsConvertibleBackToString()
         {
-            // This template is using the internal syntax, full form syntax, without omitting any spaces or #{else} statements.
-            // For example, #{if test}#{/if} has a full syntax of #{if test}#{else}#{/if}
+            // This test verifies that it is possible to convert a parsed template ToString
+            // And then find individual tokens within the template by calling ToString on each one individually
+            
             var template = @"
                 #{var}
                 #{filter | Format #{option}}
                 #{nestedParent[#{nestedChild}]}
-                #{if ifvar}#{if ifnested}#{else}debug#{/if}#{else}#{/if}
-                #{if comparison != ""value"" }true#{else}#{/if}
+                #{if ifvar}#{if ifnested}#{else}debug#{/if}#{/if}
+                #{if comparison != ""value"" }true#{/if}
                 #{each item in List}
                    #{item.Address}
                 #{/each}
                 ##{ignored}
+                #{MyVar | Match ""a b""}
+                #{MyVar | StartsWith Ab}
+                #{MyVar | Match ""ab[0-9]+""}
+                #{MyVar | Match #{pattern}}
+                #{MyVar | Contains "" b(""}
+                #{ | NowDateUtc}
+                #{MyVar | UriPart IsFile}
             ";
 
             TemplateParser.TryParseTemplate(template, out var parsedTemplate, out string error).Should().BeTrue();
             string.IsNullOrEmpty(error).Should().BeTrue();
             parsedTemplate.Should().NotBeNull();
-            parsedTemplate.ToString().Should().Be(template);
+            string templateConvertedBackToString = parsedTemplate.ToString();
+            foreach (var templateToken in parsedTemplate.Tokens)
+            {
+                if (!string.IsNullOrWhiteSpace(templateToken.ToString()))
+                    templateConvertedBackToString = templateConvertedBackToString.Replace(templateToken.ToString(),  "");
+            }
+
+            templateConvertedBackToString = templateConvertedBackToString.Replace("\r\n", "");
+            templateConvertedBackToString.Trim().Should().BeEmpty();
         }
     }
 }
