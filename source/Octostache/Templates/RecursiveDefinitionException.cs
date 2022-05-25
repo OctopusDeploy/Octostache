8eb579c530ba19c7e3e32f270901bdae0a5051fc
Matt Hilton <matt@hiltons.id>
2022-05-25 13:56:09 +1000

    Fixes typo in RecursiveDefinitionException message

--- source/Octostache/Templates/RecursiveDefinitionException.cs
+++ source/Octostache/Templates/RecursiveDefinitionException.cs
@@ -6,7 +6,7 @@ namespace Octostache.Templates
 {
     public class RecursiveDefinitionException : InvalidOperationException
     {
-        const string MessageTemplate = "An attempt to parse the variable symbol \"{0}\"  appears to have resulted in a self referencing loop ({1} -> {0}). Ensure that recursive loops do not exist in the variable values.";
+        const string MessageTemplate = "An attempt to parse the variable symbol \"{0}\" appears to have resulted in a self referencing loop ({1} -> {0}). Ensure that recursive loops do not exist in the variable values.";
 
         internal RecursiveDefinitionException(SymbolExpression symbol, Stack<SymbolExpression> ancestorSymbolStack)
             : base(string.Format(MessageTemplate, symbol, string.Join(" -> ", ancestorSymbolStack.Reverse().Select(x => x.ToString()))))
