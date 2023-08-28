26376204b9308b43ab9d4d53606bbf7a9b2cd011
isaac <isaac.calligeros@octopus.com>
2023-08-28 10:42:09 +0930

    Fixes issue where the calc operation treats multiply as whole numbers only

--- source/Octostache.Tests/CalculationFixture.cs
+++ source/Octostache.Tests/CalculationFixture.cs
@@ -33,6 +33,16 @@ namespace Octostache.Tests
             yield return new object[] { "A+2", "7" };
             yield return new object[] { "A+B", "12" };
             yield return new object[] { "C+2", "#{C+2}" };
+            // In the TemplateParser.cs for Identifier and IdentifierWithoutWhitespace we include / and - as part of the identifier.
+            // There isn't a clean fix for this, but exlcuding these in the context of a calc operation would resolve this issue (and introduce problems for variables using - and / in calc operations).
+            //yield return new object[] { "B-2", "5" };
+            yield return new object[] { "2-B", "-5" };
+            yield return new object[] { "(B*2)-2", "12" };
+            yield return new object[] { "2-(B*2)", "-12" };
+            //yield return new object[] { "B/2", (7d / 2).ToString(CultureInfo.CurrentCulture) };
+            yield return new object[] { "2/B", (2d / 7).ToString(CultureInfo.CurrentCulture) };
+            yield return new object[] { "0.2*B", (7d * 0.2).ToString(CultureInfo.CurrentCulture) };
+            yield return new object[] { "B*0.2", (7d * 0.2).ToString(CultureInfo.CurrentCulture) };
         }
     }
 }
